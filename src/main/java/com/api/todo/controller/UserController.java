d4c9b26d631b3232c7ebc18eb43b99f58231c18e
unknown <thanhnhahuynh1509@gmail.com>
2021-08-22 19:01:29 +0700

    add cross origin

--- src/main/java/com/api/todo/controller/UserController.java
+++ src/main/java/com/api/todo/controller/UserController.java
@@ -6,6 +6,7 @@ import com.api.todo.service.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
